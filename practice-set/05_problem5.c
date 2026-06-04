5577970f59b83e69442759b0e4ccca8981e7e56f
akshitkalal <akshittkalal@gmail.com>
2026-06-04 19:58:45 +0530

    Add files via upload

---
+++ practice-set/05_problem5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   float p = 47.5;
+   float r = 4;                              /*fixed data= direct  value
+                                 scanf = user se data lena 
+                            */
+   float t = 2;
+   printf("the value of simpale intrest is %f",( p* r*t)/100);
+
+    return 0;
+}
