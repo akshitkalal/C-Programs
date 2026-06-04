27c96c43eb469a63fc8d4e9ad38981b83c2f06ce
akshitkalal <akshittkalal@gmail.com>
2026-06-04 20:11:01 +0530

    Add variable declaration and print values in C
    This program demonstrates variable declaration, initialization, and printing their values.

---
+++ chapter 2/01_variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a=10 ;// declaration and initialization of variable a
+    int b = a ;
+    int c=3,d=4,n=6,o=5;
+    int c3= o + c-3;
+
+//%d for int format specifier
+// %d is for int, %f is for float and %c is for character
+
+printf("the value of a is %d \n the value of b is %d \n",a,b);
+printf("the value of d is %d \n the value of n is %d \n",d,n);
+printf("the value of c is %d \n the value of o is %d \n",c,o);
+
+    return 0;   
+}
