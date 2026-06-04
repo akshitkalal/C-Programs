90ce4157d3ebc9c66ea1b8e26d4e7a66a801ad16
akshitkalal <akshittkalal@gmail.com>
2026-06-04 19:56:52 +0530

    Create 01_problem1.c

---
+++ practice-set/01_problem1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  // int length = 6;
+   // int breadth = 3;
+   int length ,breadth;
+   
+  printf("enter length\n");
+       scanf("%d", & length);
+     printf("enter breadth/n");
+       scanf("%d", &breadth);
+    printf("the area of thise ractagler %d",length*breadth);
+    
+    return 0;
+} 
