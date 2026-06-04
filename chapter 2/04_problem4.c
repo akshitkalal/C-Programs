7d3723780b1d01ea471ee9afdd3d7f4aa78f8153
akshitkalal <akshittkalal@gmail.com>
2026-06-04 20:12:00 +0530

    Add files via upload

---
+++ chapter 2/04_problem4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float c1 = 47.0,f1;  
+    float c2 =23.0,f2;
+    
+
+       f1=  ((9.0/5.0 )*c1) +32; 
+       f2 =((9.0/5.0)*c2) +32;
+      printf("the value of fahrenheit is %f\n" ,f1);
+      printf("the value of fahrenheit is %f" ,f2);
+    return 0;
+}
