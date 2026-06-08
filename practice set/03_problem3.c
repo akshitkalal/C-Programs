ffdb315418e78832c2418bdc71846b5d2ab66bcf
akshitkalal <akshittkalal@gmail.com>
2026-06-08 17:39:56 +0530

    Add files via upload

---
+++ practice set/03_problem3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3456498;// this no is totally divide by 97 ans is 0
+    int b =6464773;// ye puri tarah se divide nahi h ans is 14
+printf("the value of a is %d\n",a%97);
+printf("the value of b is %d",b%97);
+
+    return 0;
+}
