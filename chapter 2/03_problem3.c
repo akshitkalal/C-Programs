1180b395ae87aedc77c49e6dc2bff549448e5e61
akshitkalal <akshittkalal@gmail.com>
2026-06-08 17:36:55 +0530

    Delete chapter 2/03_problem3.c

--- chapter 2/03_problem3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int r=6; 
-        int height=10;
-    printf("the area of circle whith redius %d is %f\n",r,3.14*r*r);  // isme hame %d,%f jesi chijo ka dyan rakhna chaiye
-           printf("the volume of cylinder whith redius %d and height %d is %f",r,height,3.14*r*r*height);
-    return 0;
-} 
