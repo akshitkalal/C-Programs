faa2231c12c4ab2169456df394407237883ba913
akshitkalal <akshittkalal@gmail.com>
2026-06-29 10:57:15 +0530

    Implement age condition checks in 01_conditinols.c
    Added a program to check age conditions and divisibility by 4.

---
+++ chapter 3/01_conditinols.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int age=10;
+    if(age>=5){
+        printf("we are inside if\n");      // if, if else  bhi h concapt pura hame batata h ki hamari condition 
+        printf("your  age is grater than 5\n");
+    }
+        //true hai ya false hai, aur hamari condition true hone par kya hoga aur false hone par kya hoga .
+
+    // if(age<=18){
+    //    printf("we are inside else if\n"); 
+    //    printf("your  age is grater than\n 18");
+   // } 
+   
+   if (age%4==0){
+    printf("we are inside  if\n");
+    printf("your  age is divisible by 4\n");
+
+    } 
+    return 0;
+} 
